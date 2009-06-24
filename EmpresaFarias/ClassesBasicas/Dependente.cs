4af3ffb82b1b13ca84cddd84fb8c1b3a16869a3e
jefferson.barnabe@gmail.com <jefferson.barnabe@gmail.com@993c8cc8-6067-11de-8a4b-256bef446c6c>
2009-06-24 15:37:55 +0000

    Iniciando a criação das Classes Basicas.

---
+++ EmpresaFarias/ClassesBasicas/Dependente.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassesBasicas
+{
+    /// <summary>
+    /// Classe que representa os Dependentes que um Titular poderá ter.
+    /// </summary>
+    public class Dependente
+    {
+        private int id;
+        /// <summary>
+        /// Propriedade relacionada ao Id do Dependente.
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private string nome;
+        /// <summary>
+        /// Propriedade relacionada ao Nome do Dependente.
+        /// </summary>
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private string religiao;
+        /// <summary>
+        /// Propriedade relacionada a Religiao do Dependente.
+        /// </summary>
+        public string Religiao
+        {
+            get { return religiao; }
+            set { religiao = value; }
+        }
+
+        private DateTime dataNascimento;
+        /// <summary>
+        /// Propriedade relacionada a Data de Nascimento do Dependente.
+        /// </summary>
+        public DateTime DataNascimento
+        {
+            get { return dataNascimento; }
+            set { dataNascimento = value; }
+        }
+
+        private string parentesco;
+        /// <summary>
+        /// Propriedade relacionada ao Parentesco do Dependente.
+        /// </summary>
+        public string Parentesco
+        {
+            get { return parentesco; }
+            set { parentesco = value; }
+        }
+        private int percentualCobertura;
+        /// <summary>
+        /// Propriedade relacionada ao Percentual de Cobertura do Dependente.
+        /// </summary>
+        public int PercentualCobertura
+        {
+            get { return percentualCobertura; }
+            set { percentualCobertura = value; }
+        }
+
+        private string status;
+        /// <summary>
+        /// Propriedade relacionada ao Status do Dependente.
+        /// </summary>
+        public string Status
+        {
+            get { return status; }
+            set { status = value; }
+        }       
+     }
+}
---
+++ EmpresaFarias/ClassesBasicas/Usuario.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassesBasicas
+{
+    /// <summary>
+    /// Classe que representa um Usuario que poderá acessar o Sistema.
+    /// </summary>
+    public class Usuario
+    {
+        private int id;
+        /// <summary>
+        /// Propriedade relacionada ao Id do Usuario.
+        /// </summary>
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String login;
+        /// <summary>
+        /// Propriedade relacionada ao Login do Usuario.
+        /// </summary>
+        public String Login
+        {
+            get { return login; }
+            set { login = value; }
+        }
+        private String senha;
+        /// <summary>
+        /// Propriedade relacionada a Senha do Usuario.
+        /// </summary>
+        public String Senha
+        {
+            get { return senha; }
+            set { senha = value; }
+        }
+
+        private String nome;
+        /// <summary>
+        /// Propriedade relacionada ao Nome do Usuario.
+        /// </summary>
+        public String Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        private List<Perfil> perfis;
+        /// <summary>
+        /// Propriedade relacionada aos Perfis do Usuario.
+        /// </summary>
+        public List<Perfil> Perfis
+        {
+            get { return perfis; }
+            set { perfis = value; }
+        }
+
+        /// <summary>
+        /// Construtor padrão do Usuario, inicializando os Perfis.
+        /// </summary>
+        public Usuario()
+        {
+            this.perfis = new List<Perfil>();
+        }
+    }
+}
