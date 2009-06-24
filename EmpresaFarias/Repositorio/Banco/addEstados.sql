92072f2fcd751d6460fa79d32b23dafe1bbfcfbc
jefferson.barnabe@gmail.com <jefferson.barnabe@gmail.com@993c8cc8-6067-11de-8a4b-256bef446c6c>
2009-06-24 02:56:46 +0000

    Importe Inicial

---
+++ EmpresaFarias/Repositorio/Banco/addEstados.sql
@@ -0,0 +1,134 @@
+insert into estados
+(Id,Nome,Sigla)
+values
+(1,'Acre','AC')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(2,'Alagoas','AL')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(3,'Amapá','AP')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(4,'Amazonas','AM')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(5,'Bahia','BA')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(6,'Ceará','CE')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(7,'Distrito Federal','DF')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(8,'Espírito Santo','ES')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(9,'Goiás','GO')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(10,'Maranhão','MA')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(11,'Mato Grosso','MT')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(12,'Mato Grosso do Sul','MS')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(13,'Minas Gerais','MG')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(14,'Pará','PA')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(15,'Paraíba','PB')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(16,'Paraná','PR')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(17,'Pernambuco','PE')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(18,'Piauí','PI')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(19,'Rio de Janeiro','RJ')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(20,'Rio Grande do Norte','RN')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(21,'Rio Grande do Sul','RS')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(22,'Rondônia','RO')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(23,'Roraima','RR')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(24,'Santa Catarina','SC')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(25,'São Paulo','SP')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(26,'Sergipe','SE')
+
+insert into estados
+(Id,Nome,Sigla)
+values
+(27,'Tocantins','TO')
---
+++ EmpresaFarias/Repositorio/UtilDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Repositorio
+{   /// <summary>
+    /// Classe Reponsavel por Obter uma Conexao com o Banco de Dados.
+    /// </summary>
+    public class UtilBD
+    {
+        /// <summary>
+        /// String do Conexao.
+        /// </summary>
+        private static string conexao = "Data Source=RUINOSUS-PC\\SQLEXPRESS;Initial Catalog=DBEmpresaFarias;Integrated Security=True";
+        private SqlConnection ConexaoSQL = new SqlConnection(conexao);
+        /// <summary>
+        /// Metodo para obter uma conexao.
+        /// </summary>
+        /// <returns>Uma nova conexao ao Banco de Dados.</returns>
+        public SqlConnection ObterConexao()
+        {
+             return ConexaoSQL;
+        }
+        /// <summary>
+        /// Metodo para fechar uma conexao.
+        /// </summary>
+        /// <param name="con">Conexao a ser fechada.</param>
+        public void FecharConexao(SqlConnection con)
+        {
+            if (con != null)
+            {
+                 con.Close();
+            }
+        }
+
+    }
+}
